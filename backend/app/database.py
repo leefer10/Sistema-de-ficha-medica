1708b54f980202a373d4a013c1a55bf3ce1a7af1
leefer10 <leam.ruizp@ug.edu.ec>
2026-03-21 15:12:26 -0500

    cambio en la database para coneccion de base de datos

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:2002@localhost/ficha_medica_db"
+# Leer DATABASE_URL de las variables de entorno
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:2002@localhost/ficha_medica_db")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
-
